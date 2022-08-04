fdc122fedc4d0db15d7a1120d89bb5d5c7293653
gift56 <efeasiughu@gmail.com>
2022-08-04 14:07:21 +0100

    created data

---
+++ src/Data/Cardcontent.js
@@ -0,0 +1,24 @@
+import cardImg from "../agroImg/mechl.png";
+import cardImg2 from "../agroImg/manUi.png";
+import cardImg3 from "../agroImg/shipl.png";
+
+export const CardData = [
+  {
+    img: cardImg,
+    title: "Machinery Provider",
+    details:
+      "We provide a platform where you can hire various machines and equipments for both long and short term",
+  },
+  {
+    img: cardImg2,
+    title: "Reliable Manufacturers",
+    details:
+      "We are able to supply a wide range of machinery from reputable manufactures such as, New Holland, John Deere, JCB, Case and Massey Ferguson, to name a few.",
+  },
+  {
+    img: cardImg3,
+    title: "Export Services",
+    details:
+      "We offer export services on our machinery to our international customers. We can take care of the paperwork",
+  },
+];

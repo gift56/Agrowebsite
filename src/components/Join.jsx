5ab758690bb270cc4fa68398fbe3c195d0feab22
gift56 <efeasiughu@gmail.com>
2022-08-04 15:56:00 +0100

    about stying contaent

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -1,19 +1,35 @@
 import React from "react";
 import { Container } from "./styled/Container.styled";
-import { JoinSection, Imgarea, Joinarea,Title } from "./styled/Join.styled";
+import {
+  JoinSection,
+  Imgarea,
+  Joinarea,
+  Title,
+  Detail,
+  Joincontainer,
+} from "./styled/Join.styled";
 import Crank from "../agroImg/joinImg.png";
 
 const Join = () => {
   return (
     <JoinSection>
       <Container>
-        <Imgarea>
-          <img src={Crank} alt="/" />
-        </Imgarea>
-        <Joinarea>
-            <Title></Title>
-
-        </Joinarea>
+        <Joincontainer>
+          <Imgarea>
+            <img src={Crank} alt="/" />
+          </Imgarea>
+          <Joinarea>
+            <Title>New to AgroMech?</Title>
+            <Detail>
+              Then join our mailing list to get weekly updates of new stocks and
+              offers
+            </Detail>
+            <form>
+              <input type="text" />
+              <button className="btn">Submit</button>
+            </form>
+          </Joinarea>
+        </Joincontainer>
       </Container>
     </JoinSection>
   );
--- src/components/styled/Join.styled.js
+++ src/components/styled/Join.styled.js
@@ -1,9 +1,17 @@
 import styled from "styled-components";
 
-export const JoinSection = styled.section``;
+export const JoinSection = styled.section`
+  background-color: ${({ theme }) => theme.Greenbg};
+  padding: 10px 0;
+`;
+
+export const Joincontainer = styled.div``;
 
 export const Imgarea = styled.div``;
 
 export const Joinarea = styled.div``;
 
 export const Title = styled.h2``;
+
+export const Detail = styled.p``;
+

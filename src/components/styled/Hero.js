b0c8c088a41be903f1a77cdb3c258cd674269bd4
gift56 <efeasiughu@gmail.com>
2022-08-04 11:07:15 +0100

    done with section two

--- src/components/styled/Hero.js
+++ src/components/styled/Hero.js
@@ -6,4 +6,8 @@ export const Herosec = styled.div`
   background-repeat: no-repeat;
   background-size: cover;
   height: 580px;
+  @media screen and (max-width: 450px) {
+    background-position-x: -146px;
+    height: 500px;
+  }
 `;

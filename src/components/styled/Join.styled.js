a44654e892067285919745c1bb71ad7731b59505
gift56 <efeasiughu@gmail.com>
2022-08-04 15:58:15 +0100

    stying the  contaent

--- src/components/styled/Join.styled.js
+++ src/components/styled/Join.styled.js
@@ -2,16 +2,21 @@ import styled from "styled-components";
 
 export const JoinSection = styled.section`
   background-color: ${({ theme }) => theme.Greenbg};
+`;
+
+export const Joincontainer = styled.div`
+  display: flex;
+  gap: 3rem;
+  align-items: center;
   padding: 10px 0;
 `;
 
-export const Joincontainer = styled.div``;
-
 export const Imgarea = styled.div``;
 
-export const Joinarea = styled.div``;
+export const Joinarea = styled.div`
+  flex: 1;
+`;
 
 export const Title = styled.h2``;
 
 export const Detail = styled.p``;
-

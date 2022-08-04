d292d952530aa5c8a41a207aa7c3445564702cca
gift56 <efeasiughu@gmail.com>
2022-08-04 14:50:33 +0100

    created features data

--- src/Data/Feature.js
+++ src/Data/Feature.js
@@ -1,9 +1,9 @@
 import Truck1 from "../agroImg/truck1.png";
-import Truck2 from "../agroImg/truck1.png";
-import Truck3 from "../agroImg/truck1.png";
-import Truck4 from "../agroImg/truck1.png";
-import Truck5 from "../agroImg/truck1.png";
-import Truck6 from "../agroImg/truck1.png";
+import Truck2 from "../agroImg/truck2.png";
+import Truck3 from "../agroImg/truck3.png";
+import Truck4 from "../agroImg/truck4.png";
+import Truck5 from "../agroImg/truck5.png";
+import Truck6 from "../agroImg/truck6.png";
 
 export const FeatureData = [
   {
@@ -12,6 +12,222 @@ export const FeatureData = [
       {
         title: "John Deere",
         price: "$35,000",
+      },
+    ],
+  },
+  {
+    img: Truck2,
+    greenCard: [
+      {
+        title: "New Holland 6640 TL",
+        price: "$60,000",
+      },
+    ],
+  },
+  {
+    img: Truck3,
+    greenCard: [
+      {
+        title: "Sonalika DI 75 RX Tractor",
+        price: "$30,000",
+      },
+    ],
+  },
+  {
+    img: Truck4,
+    greenCard: [
+      {
+        title: "Mahindra",
+        price: "$40,000",
+      },
+    ],
+  },
+  {
+    img: Truck5,
+    greenCard: [
+      {
+        title: "Massey Ferguson",
+        price: "$50,000",
+      },
+    ],
+  },
+  {
+    img: Truck6,
+    greenCard: [
+      {
+        title: "New Holland 6640 TL",
+        price: "$40,000",
+      },
+    ],
+  },
+  {
+    img: Truck3,
+    greenCard: [
+      {
+        title: "Sonalika DI 75 RX Tractor ",
+        price: "$30,000",
+      },
+    ],
+  },
+  {
+    img: Truck1,
+    greenCard: [
+      {
+        title: "John Deere",
+        price: "$35,000",
+      },
+    ],
+  },
+  {
+    img: Truck5,
+    greenCard: [
+      {
+        title: "Massey Ferguson",
+        price: "$50,000",
+      },
+    ],
+  },
+  {
+    img: Truck4,
+    greenCard: [
+      {
+        title: "Mahindra",
+        price: "$40,000",
+      },
+    ],
+  },
+  {
+    img: Truck1,
+    greenCard: [
+      {
+        title: "John Deere",
+        price: "$35,000",
+      },
+    ],
+  },
+  {
+    img: Truck2,
+    greenCard: [
+      {
+        title: "New Holland 6640 TL",
+        price: "$60,000",
+      },
+    ],
+  },
+  {
+    img: Truck3,
+    greenCard: [
+      {
+        title: "Sonalika DI 75 RX Tractor",
+        price: "$30,000",
+      },
+    ],
+  },
+  {
+    img: Truck4,
+    greenCard: [
+      {
+        title: "Mahindra",
+        price: "$40,000",
+      },
+    ],
+  },
+  {
+    img: Truck5,
+    greenCard: [
+      {
+        title: "Massey Ferguson",
+        price: "$50,000",
+      },
+    ],
+  },
+  {
+    img: Truck1,
+    greenCard: [
+      {
+        title: "John Deere",
+        price: "$35,000",
+      },
+    ],
+  },
+  {
+    img: Truck2,
+    greenCard: [
+      {
+        title: "New Holland 6640 TL",
+        price: "$60,000",
+      },
+    ],
+  },
+  {
+    img: Truck3,
+    greenCard: [
+      {
+        title: "Sonalika DI 75 RX Tractor",
+        price: "$30,000",
+      },
+    ],
+  },
+  {
+    img: Truck4,
+    greenCard: [
+      {
+        title: "Mahindra",
+        price: "$40,000",
+      },
+    ],
+  },
+  {
+    img: Truck5,
+    greenCard: [
+      {
+        title: "Massey Ferguson",
+        price: "$50,000",
+      },
+    ],
+  },
+  {
+    img: Truck6,
+    greenCard: [
+      {
+        title: "New Holland 6640 TL",
+        price: "$40,000",
+      },
+    ],
+  },
+  {
+    img: Truck3,
+    greenCard: [
+      {
+        title: "Sonalika DI 75 RX Tractor ",
+        price: "$30,000",
+      },
+    ],
+  },
+  {
+    img: Truck1,
+    greenCard: [
+      {
+        title: "John Deere",
+        price: "$35,000",
+      },
+    ],
+  },
+  {
+    img: Truck5,
+    greenCard: [
+      {
+        title: "Massey Ferguson",
+        price: "$50,000",
+      },
+    ],
+  },
+  {
+    img: Truck4,
+    greenCard: [
+      {
+        title: "Mahindra",
+        price: "$40,000",
       },
     ],
   },

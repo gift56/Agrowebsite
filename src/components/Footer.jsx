103c32df433cae003f9b4e87b9235daab0cce5b8
gift56 <efeasiughu@gmail.com>
2022-08-04 17:08:53 +0100

    done import required stuff

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import { Container } from "./styled/Container.styled";
+import { FooterArea } from "./styled/Footer.styled";
+import Logo from "../agroImg/logo.svg";
+import Twiter from "../agroImg/twitter.png";
+import Facebook from "../agroImg/facebook.png";
+import Instagram from "../agroImg/instagram.png";
+import Whatsapp from "../agroImg/chat.png";
+import Mail from "../agroImg/mail.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+    
+  return (
+    <FooterArea>
+      <Container></Container>
+    </FooterArea>
+  );
 };
 
 export default Footer;
---
+++ src/components/styled/Footer.styled.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const FooterArea = styled.footer``;
+
+export const Row = styled.div``;

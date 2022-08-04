6e269e624f9174ce24fbf6b3746770feca034249
gift56 <efeasiughu@gmail.com>
2022-08-04 11:53:04 +0100

    added line hieght

--- src/components/styled/Offer.styled.js
+++ src/components/styled/Offer.styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import Bg from "../../agroImg/thirdbg.png";
 
 export const Offerarea = styled.section`
-  padding: 80px 0 20px;
+  padding: 90px 0 20px;
   background-image: url(${Bg});
   background-repeat: no-repeat;
   background-size: cover;
@@ -11,11 +11,31 @@ export const Offerarea = styled.section`
 
 export const Welcometxt = styled.div`
   display: flex;
+  align-items: flex-end;
   justify-content: space-between;
 `;
 
 export const Leftside = styled.div`
   flex: 1.3;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  h3 {
+    color: ${({ theme }) => theme.whiteColor};
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 30px;
+  }
+  .description {
+    margin-top: 20px;
+    h3 {
+      font-weight: 700;
+      span {
+        display: block;
+      }
+    }
+  }
 `;
 
 export const Rightside = styled.div`

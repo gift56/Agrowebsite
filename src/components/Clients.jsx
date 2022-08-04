73a86020431cd16828d3b84f97f752b8b7eca222
gift56 <efeasiughu@gmail.com>
2022-08-04 11:27:09 +0100

    done working on clients side

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import { clients } from "../Data/Clients";
+import { Clientarea } from "./styled/clients.styled";
+import { Container } from "./styled/Container.styled";
 
 const Clients = () => {
-  return <div>Clients</div>;
+  return (
+    <Clientarea>
+      <Container>
+        <div className="client">
+          {clients.map((client, index) => (
+            <img src={client} alt="" key={index} />
+          ))}
+        </div>
+      </Container>
+    </Clientarea>
+  );
 };
 
 export default Clients;
---
+++ src/components/styled/clients.styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Clientarea = styled.div`
+  padding: 10px 0;
+  .client {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+`;

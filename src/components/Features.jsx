685ca7e1c953eeb4220b5ccd688d52247870c9f8
gift56 <efeasiughu@gmail.com>
2022-08-04 14:54:30 +0100

    mapping feature data

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { Featuresarea, Title } from "./styled/Features.styled";
+import {
+  Featuresarea,
+  Title,
+  Featurecontainer,
+  Featurecards,
+} from "./styled/Features.styled";
 import { Container } from "./styled/Container.styled";
 import { FeatureData } from "../Data/Feature";
 
@@ -8,7 +13,11 @@ const Features = () => {
     <Featuresarea>
       <Container>
         <Title>Featured Products</Title>
-        
+        <Featurecontainer>
+          {FeatureData.map((feature, index) => (
+            <Featurecards key={index}></Featurecards>
+          ))}
+        </Featurecontainer>
       </Container>
     </Featuresarea>
   );

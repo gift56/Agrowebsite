baac66d7d0916abaa1884a6b2f5d0441b077552c
gift56 <efeasiughu@gmail.com>
2022-08-04 17:38:20 +0100

    fied h3 in footer

--- src/components/styled/Footer.styled.js
+++ src/components/styled/Footer.styled.js
@@ -2,7 +2,17 @@ import styled from "styled-components";
 
 export const FooterArea = styled.footer``;
 
-export const Row = styled.div``;
+export const Row = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  h3 {
+    color: ${({ theme }) => theme.Greenbg};
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 30px;
+  }
+`;
 
 export const FooterRow = styled.div`
   padding: 30px 0 30px;
@@ -10,6 +20,7 @@ export const FooterRow = styled.div`
   flex-wrap: wrap;
   justify-content: space-between;
   align-items: flex-start;
+  margin-bottom: 40px;
 `;
 
 export const SocialRow = styled.div``;

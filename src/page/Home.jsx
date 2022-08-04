b8d58b8577de44b70d22655bd1b83aeb77d99091
gift56 <efeasiughu@gmail.com>
2022-08-04 11:08:47 +0100

    working on clients side

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Clients from "../components/Clients";
 import Hero from "../components/Hero";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <Clients />
     </>
   );
 };

74e9d0e8c5a55bd4b33adcffeb01efbb870acdf1
gift56 <efeasiughu@gmail.com>
2022-08-05 14:00:21 +0100

    stying title area

--- src/components/styled/Choose.styled.js
+++ src/components/styled/Choose.styled.js
@@ -4,9 +4,20 @@ export const ChooseCon = styled.section`
   background-color: ${({ theme }) => theme.featureBg};
 `;
 
-export const ChooseArea = styled.div``;
+export const ChooseArea = styled.div`
+  padding: 20px 0 30px;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
 
-export const ChooseTitle = styled.div``;
+export const ChooseTitle = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+`;
 
 export const ChooseCardContainer = styled.div``;
 

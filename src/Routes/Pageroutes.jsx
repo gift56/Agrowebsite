66a4a4517c5719da613da2c166a336dadabfe3bc
gift56 <efeasiughu@gmail.com>
2022-08-05 15:04:20 +0100

    working on another age

--- src/Routes/Pageroutes.jsx
+++ src/Routes/Pageroutes.jsx
@@ -4,6 +4,7 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Home from "../page/Home";
 import About from "../page/About";
+import Service from "../page/Service";
 
 const Pageroutes = () => {
   return (
@@ -12,6 +13,7 @@ const Pageroutes = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="service" element={<Service />} />
       </Routes>
       <Footer />
     </>

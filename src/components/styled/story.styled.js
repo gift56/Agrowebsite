f8f05df98dc44ffc12fb33b568626099e1469fb6
gift56 <efeasiughu@gmail.com>
2022-08-05 13:06:04 +0100

    stying the h4 in text area

--- src/components/styled/story.styled.js
+++ src/components/styled/story.styled.js
@@ -5,7 +5,7 @@ export const StoryCon = styled.section`
 `;
 
 export const Storyarea = styled.div`
-  padding: 40px 0 40px;
+  padding: 60px 0 40px;
   display: flex;
   justify-content: center;
   align-items: flex-start;
@@ -21,6 +21,21 @@ export const Textside = styled.div`
     font-weight: 600;
     font-size: 28px;
     line-height: 42px;
+    position: relative;
+    color: ${({ theme }) => theme.Greenbg};
+    &::before {
+      content: "";
+      position: absolute;
+      top: -9px;
+      width: 100px;
+      height: 2px;
+      background-color: #45914a;
+    }
+  }
+  h4 {
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 30px;
   }
 `;
 

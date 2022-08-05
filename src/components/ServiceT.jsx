2a895aa770dbae369ee8907168605f64f5fcb767
gift56 <efeasiughu@gmail.com>
2022-08-05 15:31:23 +0100

    adding some img

--- src/components/ServiceT.jsx
+++ src/components/ServiceT.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container } from "./styled/Container.styled";
 import { ServiceArea, ServiceCon, TitleServe } from "./styled/Service.styled";
+import Line from "../agroImg/line.png";
 
 const ServiceT = () => {
   return (
@@ -8,9 +9,10 @@ const ServiceT = () => {
       <Container>
         <ServiceArea>
           <TitleServe>
+            <img src={Line} alt="/" />
             <h2>Our Service</h2>
+            <img src={Line} alt="/" />
           </TitleServe>
-          
         </ServiceArea>
       </Container>
     </ServiceCon>

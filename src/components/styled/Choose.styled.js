2e1148a78ce0902c554aba0874b50d13ad05a376
gift56 <efeasiughu@gmail.com>
2022-08-05 14:02:20 +0100

    done with title

--- src/components/styled/Choose.styled.js
+++ src/components/styled/Choose.styled.js
@@ -17,6 +17,18 @@ export const ChooseTitle = styled.div`
   justify-content: center;
   align-items: center;
   gap: 1rem;
+  h2 {
+    font-weight: 600;
+    font-size: 28px;
+    line-height: 42px;
+    color: ${({ theme }) => theme.textColor};
+  }
+  p {
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 34px;
+    color: ${({ theme }) => theme.textColor};
+  }
 `;
 
 export const ChooseCardContainer = styled.div``;

8c0cedec64afb3a61d31a16f9d521061e8bf2f4c
gift56 <efeasiughu@gmail.com>
2022-08-05 17:31:34 +0100

    made some shanges

--- src/page/About.jsx
+++ src/page/About.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import AboutBg from "../agroImg/aboutImg.png";
 import AboutClients from "../components/AboutClients";
+import { AboutHsec } from "../components/styled/Hero";
 import AboutH from "../components/AboutH";
 import Choose from "../components/Choose";
 import Story from "../components/Story";
-import { AboutHsec } from "../components/styled/Hero";
 import Join from "../components/Join";
 
 const About = () => {
--- src/page/Gallery.jsx
+++ src/page/Gallery.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { AboutHsec } from "../components/styled/Hero";
+import AboutH from "../components/AboutH";
+import GalleryBg from "../agroImg/galleryBg.png";
 
 const Gallery = () => {
-  return <div>Galerry</div>;
+  return (
+    <>
+      <AboutHsec bg={GalleryBg}>
+        <AboutH title="Image Gallery" detailsT="Gallery " />
+      </AboutHsec>
+    </>
+  );
 };
 
 export default Gallery;

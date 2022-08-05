bd57f81fd635814c55b567d867a0ed2e907e000c
gift56 <efeasiughu@gmail.com>
2022-08-05 17:33:23 +0100

    made some changes

---
+++ src/components/GalleryImgs.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GalleryImgs = () => {
+  return (
+    <div>GalleryImgs</div>
+  )
+}
+
+export default GalleryImgs
--- src/page/Gallery.jsx
+++ src/page/Gallery.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { AboutHsec } from "../components/styled/Hero";
 import AboutH from "../components/AboutH";
 import GalleryBg from "../agroImg/galleryBg.png";
+import GalleryImgs from "../components/GalleryImgs";
 
 const Gallery = () => {
   return (
@@ -9,6 +10,7 @@ const Gallery = () => {
       <AboutHsec bg={GalleryBg}>
         <AboutH title="Image Gallery" detailsT="Gallery " />
       </AboutHsec>
+      <GalleryImgs />
     </>
   );
 };

8e633f03a47ab6a272e9b34e019bc7fafeef3de9
gift56 <efeasiughu@gmail.com>
2022-08-06 12:57:40 +0100

    done with button

--- src/components/styled/PartM.styled.js
+++ src/components/styled/PartM.styled.js
@@ -25,6 +25,29 @@ export const PleftSide = styled.div`
     height: 658px;
     box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
     border-radius: 8px 8px 95px 8px;
+    h2 {
+      font-weight: 600;
+      font-size: 25px;
+      line-height: 38px;
+      letter-spacing: -0.011em;
+      color: #474747;
+    }
+    p {
+      font-weight: 500;
+      font-size: 15px;
+      line-height: 37px;
+      text-align: justify;
+      letter-spacing: -0.011em;
+      color: ${({ theme }) => theme.textColor};
+      span {
+        display: block;
+      }
+    }
+    button {
+      background-color: #efe503;
+      border-color: #efe503;
+      color: #45914A;
+    }
   }
 `;
 

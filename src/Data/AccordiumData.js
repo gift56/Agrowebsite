ae79f16059022dbc49b021334810e6e108769e4b
gift56 <efeasiughu@gmail.com>
2022-08-06 17:12:19 +0100

    done with data

---
+++ src/Data/AccordiumData.js
@@ -0,0 +1,37 @@
+export const Accordium = [
+  {
+    questions: "How do i place an order ?",
+    answer:
+      "You can place an order by simply registering on the website and searching for the product you want",
+  },
+  {
+    questions: "What are your shipping policies ?",
+    answer:
+      "You can place an order by simply registering on the website and searching for the product you want",
+  },
+  {
+    questions: "Can i change my address later ?",
+    answer:
+      "You can place an order by simply registering on the website and searching for the product you want",
+  },
+  {
+    questions: "Are there warranty on your products ?",
+    answer:
+      "You can place an order by simply registering on the website and searching for the product you want",
+  },
+  {
+    questions: "What is the estimated time of arrival ?",
+    answer:
+      "You can place an order by simply registering on the website and searching for the product you want",
+  },
+  {
+    questions: "Do you offer any discount ?",
+    answer:
+      "You can place an order by simply registering on the website and searching for the product you want",
+  },
+  {
+    questions: "What is your refund policy",
+    answer:
+      "You can place an order by simply registering on the website and searching for the product you want",
+  },
+];

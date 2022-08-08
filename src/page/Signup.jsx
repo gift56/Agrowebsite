5a6df4bdb6a2db0fbaa10527b86b5665bdb35231
gift56 <efeasiughu@gmail.com>
2022-08-08 16:21:47 +0100

    stying the sigup section

--- src/page/Signup.jsx
+++ src/page/Signup.jsx
@@ -5,13 +5,53 @@ import {
 } from "../components/styled/Signup.styled";
 import { Container } from "../components/styled/Container.styled";
 import Createimg from "../agroImg/createImg.png";
+import Logo from "../agroImg/logo.svg";
+import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
 
 const Signup = () => {
   return (
     <Signuparea>
       <Container>
         <SignupFormcontainer>
-          <div className="leftside"></div>
+          <div className="leftside">
+            <div className="logo">
+              <img src={Logo} alt="/" />
+            </div>
+            <h2>Create Account</h2>
+            <form>
+              <div className="formControl">
+                <label htmlFor="email">Email Address</label>
+                <input type="text" placeholder="Enter your email" />
+              </div>
+              <div className="formControl">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="text"
+                  id="password"
+                  placeholder="*********************"
+                />
+                <div className="show">
+                  <AiFillEyeInvisible />
+                  <AiFillEye />
+                </div>
+              </div>
+              <div className="formControl">
+                <label htmlFor="confirmPassword">Confirm Password</label>
+                <input
+                  type="text"
+                  id="confirmPassword"
+                  placeholder="*********************"
+                />
+                <div className="show">
+                  <AiFillEyeInvisible />
+                  <AiFillEye />
+                </div>
+              </div>
+            </form>
+            <div className="option">
+              <p>or</p>
+            </div>
+          </div>
           <div className="rightside"></div>
         </SignupFormcontainer>
       </Container>

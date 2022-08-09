f60b9ecb0477ca9685ffe89bc7f748f55c01c0e5
gift56 <efeasiughu@gmail.com>
2022-08-09 11:51:54 +0100

    still fixing issues

--- src/components/styled/Signup.styled.js
+++ src/components/styled/Signup.styled.js
@@ -1,5 +1,27 @@
 import styled from "styled-components";
 
-export const Signuparea = styled.section``;
+export const Signuparea = styled.section`
+  background: #e5e5e5;
+`;
 
-export const SignupFormcontainer = styled.div``;
+export const SignupFormcontainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  .leftside {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    gap: 1rem;
+    background-color: ${({ theme }) => theme.whiteColor};
+    width: 40%;
+    height: 700px;
+    padding: 3rem;
+  }
+  .rightside {
+    img {
+      height: 700px;
+    }
+  }
+`;

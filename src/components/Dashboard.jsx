df7cf4984b07fe58350412536ebaac0692e32415
gift56 <efeasiughu@gmail.com>
2022-08-12 11:23:47 +0100

    gotten some Img

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,15 +1,35 @@
 import React from "react";
-import DashboardPages from "./DashboardPages";
+import DashboardPages from "../page/DashboardPages";
 import { Container } from "./styled/Container.styled";
 import { DashboardCon, LeftSideDash } from "./styled/Dashboard.styled";
 import { ServiceCon } from "./styled/Service.styled";
+import Box from "../agroImg/square.png";
+import Pencil from "../agroImg/pencil.png";
 
 const Dashboard = () => {
   return (
     <ServiceCon>
       <Container>
         <DashboardCon>
-          <LeftSideDash></LeftSideDash>
+          <LeftSideDash>
+            <div className="sidebar">
+              <div className="fileForm">
+                <form>
+                  <label htmlFor="editProfile" className="imgLabel">
+                    <img src={Box} alt="" className="profileImg" />
+                    <img src={Pencil} alt="" className="editPen" />
+                  </label>
+                  <input
+                    type="file"
+                    id="editProfile"
+                    style={{ display: "none" }}
+                    accept="*.png *.jpg *.jpeg"
+                  />
+                </form>
+              </div>
+
+            </div>
+          </LeftSideDash>
           <DashboardPages />
         </DashboardCon>
       </Container>

c9ea39c9b422bafa34e225ded52a743221eb1c88
gift56 <efeasiughu@gmail.com>
2022-11-18 16:07:57 +0100

    updating some stuff

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -10,6 +10,7 @@ import {
 } from "./styled/Features.styled";
 import { Container } from "./styled/Container.styled";
 import { FeatureData } from "../Data/Feature";
+import { motion } from "framer-motion";
 
 const Features = () => {
   return (
@@ -20,15 +21,47 @@ const Features = () => {
           {FeatureData.map((feature, index) => (
             <Featurecards key={index}>
               <Imgcontainer>
-                <img src={feature.img} alt="/" />
+                <motion.img
+                  initial={{ opacity: 0, scale: 0 }}
+                  whileInView={{ opacity: 1, scale: 1 }}
+                  transition={{ duration: 2 }}
+                  src={feature.img}
+                  alt="/"
+                />
               </Imgcontainer>
               {feature.greenCard.map((item, index) => (
                 <Greencard key={index}>
-                  <h3>{item.title}</h3>
-                  <p>{item.price}</p>
+                  <motion.h3
+                    initial={{ opacity: 0, y: "-30px" }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 2 }}
+                  >
+                    {item.title}
+                  </motion.h3>
+                  <motion.p
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1 }}
+                    transition={{ duration: 2 }}
+                  >
+                    {item.price}
+                  </motion.p>
                   <Buttons>
-                    <button className="btn">View</button>
-                    <button className="btn">Add to Cart</button>
+                    <motion.button
+                      initial={{ opacity: 0, scale: 0 }}
+                      whileInView={{ opacity: 1, scale: 1 }}
+                      transition={{ duration: 2 }}
+                      className="btn"
+                    >
+                      View
+                    </motion.button>
+                    <motion.button
+                      initial={{ opacity: 0, scale: 0 }}
+                      whileInView={{ opacity: 1, scale: 1 }}
+                      transition={{ duration: 2 }}
+                      className="btn"
+                    >
+                      Add to Cart
+                    </motion.button>
                   </Buttons>
                 </Greencard>
               ))}

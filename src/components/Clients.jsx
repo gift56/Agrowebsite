99cda7c29474c0be3345d51413dc52094eec0833
gift56 <efeasiughu@gmail.com>
2022-11-18 14:46:16 +0100

    added animation to clients

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { clients } from "../Data/Clients";
 import { Clientarea } from "./styled/clients.styled";
 import { Container } from "./styled/Container.styled";
+import { motion } from "framer-motion";
 
 const Clients = () => {
   return (
@@ -9,7 +10,14 @@ const Clients = () => {
       <Container>
         <div className="client">
           {clients.map((client, index) => (
-            <img src={client} alt="" key={index} />
+            <motion.img
+              initial={{ opacity: 0, scale: 0 }}
+              whileInView={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 2 }}
+              src={client}
+              alt=""
+              key={index}
+            />
           ))}
         </div>
       </Container>

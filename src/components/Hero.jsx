cc0d3888ae35ef7511b98f18a5b11904da596ac6
gift56 <efeasiughu@gmail.com>
2022-11-18 14:40:38 +0100

    updated motion

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Herosec } from "./styled/Hero";
 import Homebg from "../agroImg/homeImg.png";
 import { Container } from "./styled/Container.styled";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (

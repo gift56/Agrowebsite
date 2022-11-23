5c541a33d953c090332a4ffe1ce3d629560ec7d4
gift56 <efeasiughu@gmail.com>
2022-11-23 15:36:24 +0100

    working on some stuff

--- src/components/AboutH.jsx
+++ src/components/AboutH.jsx
@@ -2,18 +2,29 @@ import React from "react";
 import { Herotext } from "./styled/Hero";
 import { IoMdHome } from "react-icons/io";
 import { BiChevronRight } from "react-icons/bi";
+import { motion } from "framer-motion";
 
 const AboutH = ({ title, detailsT }) => {
   return (
     <Herotext>
-      <h2>{title}</h2>
-      <p>
+      <motion.h2
+        initial={{ opacity: 0, y: "-50" }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 2 }}
+      >
+        {title}
+      </motion.h2>
+      <motion.p
+        initial={{ opacity: 0, scale: 0 }}
+        whileInView={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 3 }}
+      >
         <IoMdHome />
         <span>
           <BiChevronRight />
           {detailsT}
         </span>
-      </p>
+      </motion.p>
     </Herotext>
   );
 };

625be274155d9bce9255e3fe1ad73c4577b203a4
gift56 <efeasiughu@gmail.com>
2022-11-23 16:06:32 +0100

    added some animations

--- src/components/ServiceT.jsx
+++ src/components/ServiceT.jsx
@@ -4,6 +4,7 @@ import { Container } from "./styled/Container.styled";
 import { ServiceArea, ServiceCon, TitleServe } from "./styled/Service.styled";
 import { ChooseCardContainer, ChooseCard } from "./styled/Choose.styled";
 import Line from "../agroImg/line.png";
+import { motion } from "framer-motion";
 
 const ServiceT = () => {
   return (
@@ -11,9 +12,27 @@ const ServiceT = () => {
       <Container>
         <ServiceArea>
           <TitleServe>
-            <img src={Line} alt="/" />
-            <h2>Our Service</h2>
-            <img src={Line} alt="/" />
+            <motion.img
+              initial={{ opacity: 0, x: "-50px" }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 2 }}
+              src={Line}
+              alt="/"
+            />
+            <motion.h2
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{ duration: 3 }}
+            >
+              Our Service
+            </motion.h2>
+            <motion.img
+              initial={{ opacity: 0, x: "50px" }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 2 }}
+              src={Line}
+              alt="/"
+            />
           </TitleServe>
           <ChooseCardContainer>
             {ServiceData.map((card, index) => (

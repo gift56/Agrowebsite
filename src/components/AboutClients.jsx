87c61e5b9baf4a29e439b843cf593e7fcdc71be1
gift56 <efeasiughu@gmail.com>
2022-11-23 16:02:22 +0100

    done with about us animation

--- src/components/AboutClients.jsx
+++ src/components/AboutClients.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { clients2 } from "../Data/Clients";
 import { Container } from "./styled/Container.styled";
 import { Clientarea } from "./styled/clients.styled";
+import { motion } from "framer-motion";
 
 const AboutClients = () => {
   return (
@@ -9,7 +10,14 @@ const AboutClients = () => {
       <Container>
         <div className="client">
           {clients2.map((client, index) => (
-            <img src={client} alt="" key={index} />
+            <motion.img
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{ duration: 3 }}
+              src={client}
+              alt=""
+              key={index}
+            />
           ))}
         </div>
       </Container>
